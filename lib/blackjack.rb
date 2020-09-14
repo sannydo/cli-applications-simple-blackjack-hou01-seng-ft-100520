e0a85b4b2dd304e7f4317a6f06684adc806c1376
Sanny Do <sanny.do94@gmail.com>
2020-09-14 23:26:12 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,11 +23,7 @@ def end_game(card_total)
 end
 
 def initial_round
-   card_one = deal_card
-   card_two = deal_card
-   card_total = card_one + card_two
-   display_card_total(card_total)
-   return card_total
+   
 end
 
 
