0a26cd09934fbcabd7ff1da16555e43ad2870692
Sanny Do <sanny.do94@gmail.com>
2020-09-14 23:10:11 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,15 +32,15 @@ end
 
 def hit? (card_total)
   # code hit? here
-  prompt_user
-  if get_user_input == 'h'
-    card_total += deal_card
-  elsif get_user_input == 's'
-    card_total
-  else
-    invalid_command
-  end
-    card_total
+prompt_user
+if get_user_input == 'h'
+  card_total += deal_card
+elsif get_user_input == 's'
+  card_total
+else
+  invalid_command
+end
+card_total
 end
 
 
