e225335d58838ae22e41fae3c0d249a8fafb9c31
Sanny Do <sanny.do94@gmail.com>
2020-09-14 23:01:43 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,6 +35,10 @@ def hit? (card_total)
   prompt_user
   if get_user_input == 'h'
     card_total += deal_card
+  if get_user_input == 's'
+    card_total
+  if != "s" or "h"
+    prompt_user
   end
     card_total
 end
