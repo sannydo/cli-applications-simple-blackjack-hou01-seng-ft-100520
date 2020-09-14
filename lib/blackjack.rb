790b19742bf60540e851e4c08e761ed1adb3d432
Sanny Do <sanny.do94@gmail.com>
2020-09-14 23:18:05 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -64,7 +64,7 @@ def runner
 welcome
 sleep 0.75
 card_total = initial_round
-  until hand > 21
+  until card_total > 21
     card_total = hit?(card_total)
     display_card_total(card_total)
   end
